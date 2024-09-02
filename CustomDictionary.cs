e23171b1741fb0d7bc6452d5d77b79b33b4db6cc
user <user@DESKTOP-5E78IH2>
2024-09-02 16:15:25 +0200

    Add project files.

---
+++ CustomDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class CustomDictionary<TKey, TValue>
+{
+    private Dictionary<TKey, TValue> dictionary;
+
+    public CustomDictionary()
+    {
+        dictionary = new Dictionary<TKey, TValue>();
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        dictionary[key] = value;
+    }
+
+    public void Update(TKey key, TValue value)
+    {
+        if (dictionary.ContainsKey(key))
+        {
+            dictionary[key] = value;
+        }
+    }
+
+    public void Remove(TKey key)
+    {
+        dictionary.Remove(key);
+    }
+
+    public TValue Get(TKey key)
+    {
+        return dictionary.ContainsKey(key) ? dictionary[key] : default(TValue);
+    }
+
+    public bool ContainsKey(TKey key)
+    {
+        return dictionary.ContainsKey(key);
+    }
+
+    public ICollection<KeyValuePair<TKey, TValue>> GetAllItems()
+    {
+        return dictionary;
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartHomeManagement
+{
+    public partial class MainWindow : Window
+    {
+        private CustomDictionary<string, string> deviceDictionary;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            deviceDictionary = new CustomDictionary<string, string>();
+        }
+
+        private void AddDeviceButton_Click(object sender, RoutedEventArgs e)
+        {
+            string deviceId = DeviceIdTextBox.Text;
+            string status = ((ComboBoxItem)StatusComboBox.SelectedItem).Content.ToString();
+
+            if (!string.IsNullOrEmpty(deviceId) && !string.IsNullOrEmpty(status))
+            {
+                deviceDictionary.Add(deviceId, status);
+                RefreshDeviceList();
+                ClearInputs();
+            }
+            else
+            {
+                MessageBox.Show("Please enter both Device ID and Status.");
+            }
+        }
+
+        private void UpdateStatusButton_Click(object sender, RoutedEventArgs e)
+        {
+            string deviceId = DeviceIdTextBox.Text;
+            string status = ((ComboBoxItem)StatusComboBox.SelectedItem).Content.ToString();
+
+            if (deviceDictionary.ContainsKey(deviceId))
+            {
+                deviceDictionary.Update(deviceId, status);
+                RefreshDeviceList();
+                ClearInputs();
+            }
+            else
+            {
+                MessageBox.Show("Device ID not found.");
+            }
+        }
+
+        private void RemoveDeviceButton_Click(object sender, RoutedEventArgs e)
+        {
+            string deviceId = DeviceIdTextBox.Text;
+
+            if (deviceDictionary.ContainsKey(deviceId))
+            {
+                deviceDictionary.Remove(deviceId);
+                RefreshDeviceList();
+                ClearInputs();
+            }
+            else
+            {
+                MessageBox.Show("Device ID not found.");
+            }
+        }
+
+        private void RefreshDeviceList()
+        {
+            DevicesListBox.Items.Clear();
+            foreach (var item in deviceDictionary.GetAllItems())
+            {
+                DevicesListBox.Items.Add($"{item.Key}: {item.Value}");
+            }
+        }
+
+        private void ClearInputs()
+        {
+            DeviceIdTextBox.Clear();
+            StatusComboBox.SelectedIndex = -1;
+        }
+    }
+}
